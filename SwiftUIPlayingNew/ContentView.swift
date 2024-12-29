5cc8e06a2ec9708352c060d95e38fe017e27cfe9
islamSwira <islamswira93@gmail.com>
2024-12-29 16:09:56 +0200

    Creating Snow fall Animation Using SPKit for initial commit

--- SwiftUIPlayingNew/ContentView.swift
+++ SwiftUIPlayingNew/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import SpriteKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+//        GeometryReader { _ in
+            ZStack {
+                Color(.darkGray)
+                SpriteView(scene: SnowFall(), options: [.allowsTransparency])
+            }
+//        }
+//        .ignoresSafeArea()
+    }
+}
+
+
+class SnowFall: SKScene {
+    override func sceneDidLoad() {
+        size = UIScreen.main.bounds.size
+        scaleMode = .resizeFill
+        anchorPoint = CGPoint(x: 0.5, y: 1)
+        backgroundColor = .clear
+
+
+        let node = SKEmitterNode(fileNamed: "ParticleRain.sks")!
+        addChild(node)
+        node.particlePositionRange.dx = UIScreen.main.bounds.width
     }
 }
 

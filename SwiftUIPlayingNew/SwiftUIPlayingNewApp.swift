4918b16435cfe4a5384acb88181532c8672e2ec9
Islam Swira <islamswira93@gmail.com>
2024-12-29 15:48:10 +0200

    Initial Commit

---
+++ SwiftUIPlayingNew/SwiftUIPlayingNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlayingNewApp.swift
+//  SwiftUIPlayingNew
+//
+//  Created by IslamSwira on 29/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlayingNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
